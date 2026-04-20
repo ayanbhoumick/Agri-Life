3ded85bf8931c1fa13a7ed540da0b9472082fc18
Ayan Bhoumick <94864371+ayanbhoumick@users.noreply.github.com>
2026-04-20 17:51:14 +0530

    feat: add Spring Boot application entry point

---
+++ src/main/java/agrilife/AgriLifeApplication.java
@@ -0,0 +1,12 @@
+package agrilife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriLifeApplication.class, args);
+    }
+}
